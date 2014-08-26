bef307d89ab98ccc6ce11ffbebbcec6a5beff45e
h4xp0ck3tz <evilrobotfactory@gmail.com>
2014-08-26 12:45:24 -0400

    Adding Files
    Added Library Files.

---
+++ XbeeConfig.h
@@ -0,0 +1,24 @@
+ /*
+ * XbeeConfig.h
+ *
+ * Created: 7/11/2014 4:55:59 PM
+ *  Author: Waron
+ */ 
+
+
+#ifndef XBEECONFIG_H_
+#define XBEECONFIG_H_
+#include "Xbee.h"
+
+#define XBEES1
+
+int XbeeSetup()
+{
+	//setPANID(1224);	// Set PAN ID
+	
+	
+	return 1;
+}
+
+
+#endif /* XBEECONFIG_H_ */
