ae96f190d8ad1a11ea56fbaf82333488eba79422
h4xp0ck3tz <evilrobotfactory@gmail.com>
2015-02-19 19:35:04 -0500

    Update Xbee_Lib.c

--- Xbee_Lib.c
+++ Xbee_Lib.c
@@ -5,13 +5,13 @@
  *  Author: Waron
  */ 
 
-#include "Motesquito.h"
+#include "Xbee.h"
 #include <util/delay.h>
 
 
 char test[] = {'T', 'E', 'S', 'T'};
 
-void newPacket()
+void cb()
 {
 	//printf("Interrupt Received\n\r");
 	//ZigBee_TX_Request(0x00, 0, 0x00FFFF, 0, 0x01, 0x01, test, 4);
@@ -24,7 +24,8 @@ void newPacket()
 	for(i=0;i<rx_data.data_len;i++)
 	{
 		temp = (rx_data.data[i]);
-		buff[i] = temp;
+		//buff[i] = temp;
+		buff[i] = temp;	// Cipher Code
 	}
 	TX_Request_16bit(0x01, rx_data.source_addr_16bit, 0, buff, rx_data.data_len);
 	//ZigBee_TX_Request(0,0,0xFFFF,rx_data.source_addr_16bit, 1, 0, buff, rx_data.data_len);
@@ -34,7 +35,7 @@ void newPacket()
 int main(void)
 {
 	XbeeInit();
-	setNewPacketCB(newPacket);
+	setNewPacketCB(cb);
 	//printf("Hello World\n");
 	
 	/*
@@ -52,7 +53,7 @@ int main(void)
 		//TX_Request_16bit(0,0xFFFF,0,test,4);
 		
 		get16bitAddress();
-	_delay_ms(2000);1
+		_delay_ms(2000);
 		set16bitAddress(6);
 		_delay_ms(2000);
 		get16bitAddress();
@@ -65,6 +66,7 @@ int main(void)
 		_delay_ms(2000);
 		AT_Command(0x00,'W','R',0,0);
 		_delay_ms(2000);
+		
 		
     }
 }
